805b85f52aea0ae2853fff03bf0bb4c120a5999a
B11 <ibilalium@gmail.com>
2025-04-09 18:22:54 +0100

    + Configuration du frontend, Category

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -5,6 +5,8 @@ import pic from "./assets/int.jpg"
 import { Scale } from '@mui/icons-material';
 import Tete from './components/tete.jsx'
 import NavBar from './components/navbar.jsx';
+import Category from './components/Category2.jsx'
+import CategoryDiv from './components/Categorydiv.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,9 +15,9 @@ function App() {
       <div className='w-full h-full flex-col'>
         <h3>LE FRONTEND N'EST PAS ENCORE FINI </h3>
         <Tete/>
-        <Category/>
         <NavBar/>
-        <CategoryDiv/>
+        <CategoryDiv title="Favorites"/>
+        <CategoryDiv title="Drama"/>
       </div>
   )
 }
